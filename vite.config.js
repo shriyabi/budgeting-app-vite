c9892e77cbb7366872eb9a88a307ddf1a8315a9a
shriyabi <shriyarbiddala@gmail.com>
2026-01-25 19:31:52 -0700

    new v3

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //adding gh-pages: step 1
+  base: "/budgeting-app-vite/",
 })

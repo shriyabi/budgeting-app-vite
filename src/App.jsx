e2e8c4821cfd0c33e92e012446e51c195f83b0b2
shriyabi <shriyarbiddala@gmail.com>
2026-01-26 00:44:07 -0700

    adding new changes plus favicon

--- src/App.jsx
+++ src/App.jsx
@@ -300,8 +300,7 @@ export default function BudgetApp() {
   }
 
   return (
-    // Uses a subtle gradient from "Paper White" to "Mint Cream" (Dark: Charcoal to Deep Forest)
-    <div className='w-screen bg-linear-to-br from-[#fdfbf7] to-[#ecfdf5] dark:from-gray-950 dark:to-[#064e3b] h-auto'>
+    <div className='w-screen bg-linear-to-br from-[#fdfbf7] to-[#ecfdf5] dark:from-gray-950 dark:to-[#02261d] h-auto'>
       <div className="max-w-7xl mx-auto p-4 md:p-8 font-sans text-gray-900 dark:text-gray-100 min-h-screen relative transition-colors duration-300">
         <div className="flex justify-between items-center mb-10">
           <div className="flex items-center gap-3"><div className="w-10 h-10 bg-linear-to-br from-[#064e3b] to-[#10b981] rounded-lg flex items-center justify-center text-white text-3xl shadow-lg border border-[#34d399]/30">&#x1F4B0;</div><h1 className="text-2xl font-extrabold text-gray-900 dark:text-white tracking-tight font-serif">Encourage-mint</h1></div>
@@ -431,7 +430,7 @@ export default function BudgetApp() {
                 <div className="mt-2 lg:mb-10">
                   <span className={`text-5xl font-extrabold ${unallocatedFunds < 0
                     ? 'text-rose-500 drop-shadow-sm'
-                    : 'bg-linear-to-r from-emerald-600 font-sans to-teal-500 dark:from-emerald-400 dark:to-teal-300 bg-clip-text text-transparent'
+                    : 'bg-linear-to-r from-emerald-600 font-sans to-teal-500 dark:from-emerald-400 dark:to-emerald-200 bg-clip-text text-transparent'
                     }`}>
                     ${unallocatedFunds.toLocaleString()}
                   </span>
